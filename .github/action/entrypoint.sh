23b00ee54786303f63ad78e79d3ee7ff56c53483
zrma <bulbitain@gmail.com>
2021-04-11 02:23:57 +0900

    ci: add golang 1.16

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -6,6 +6,10 @@ wget --retry-connrefused --no-check-certificate -T 60 https://dl.google.com/go/g
 tar -zxf /usr/local/go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local
 ln -s /usr/local/go/bin/go /usr/local/bin/go
 
+go get -u github.com/jandelgado/gcov2lcov
+
 golangci-lint run --timeout 10m
 
 go test -race -coverprofile=coverage.out -covermode=atomic $(go list ./... | grep -v /cmd/)
+
+~/go/bin/gcov2lcov -infile=coverage.out -outfile=coverage.lcov
